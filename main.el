b3e45c1492910308bdfb0ade24466c60a5f13fd5
Duddino <duddinogatto@pm.me>
2022-02-19 15:18:38 +0100

    Fixed some things and added step-logic

--- main.el
+++ main.el
@@ -1,25 +1,29 @@
 (require 'cl-lib)
 
-(defvar operator-precedence (list 'rightarrow 'and 'or 'not))
-
 (defun string-to-operator
     (str)
+  "Takes a string a string and returns its operator if it is one, 
+   otherwise nil. For example \\rightarrow becomes 'rightarrow"
   (cond
    ((string-equal str "\\rightarrow") 'rightarrow)
    ((string-equal str "\\&") 'and)
    ((string-equal str "\\vee") 'or)
    ((string-equal str "\\neg") 'not)))
+
 (defun operator-to-precedence
     (op)
+  "Takes an operator and returns its precedence, with the highest
+   precendece being 0. The lower the precendce the higher the number."
   (cond
    ((eq 'rightarrow op) 0)
    ((eq 'and op) 1)
    ((eq 'or op) 1)
    ((eq 'not op) 2)))
 
-;; e.g. A, B \& A \vdash C, C \vee A =>
-;;(("A" "B\&A") ("C" "C \vee A"))
 (defun parse-logic (line)
+  "Parses a logic expression and returns a nested list like so: 
+   A, B, \\& A \\vdash C, C \\vee A =>
+   ((A \"B \\& A\") (C \"C \\vee A\"))"
   (mapcar
    (lambda (x)
      (unless (= (length (string-trim x)) 0)
@@ -27,6 +31,8 @@
    (split-string line "\\\\vdash")))
 
 (defun remove-outer-parentheses (str)
+  "Trims the expression of any white spaces and removes the first and last
+   if they're respectively an ( and a ) character"
   (let ((str (string-trim str)))
     (if
 	(and
@@ -36,21 +42,24 @@
 	 (substring str 1 (1- (length str))))
       str)))
 
-;; Get the lowest nested \ and then analyze it
 (defun parse-expression
     (str)
-  "Takes an expression, e.g. \" A \\& B \" and returns its parsed rapresentation (in prefix rapresentation),
-   in this example, ('and \"A\" \"B\") or itself if str is a complete expression"
-  (if
-      (is-complete-expression str)
-      (string-trim str)
-    (let* ((str (remove-outer-parentheses str))
+  "Takes an expression, e.g. \"A \\& B\" and returns its parsed rapresentation (in prefix rapresentation) or itself if str is a complete expression.
+   In the above example, ('and \"A\" \"B\")"
+  (if (listp str)
+      str ;; str has already been parsed
+    (if
+	(is-complete-expression str)
+	(string-trim str)
+      (let*
+	  ((str (remove-outer-parentheses str))
 	   (operator-delimiter
 	    (cl-reduce
 	     (lambda (x y)
-	       (if (<
+	       (if
+		   (<
 		    (operator-to-precedence (car x))
-		    (operator-to-precedence (car y)))     
+		    (operator-to-precedence (car y)))
 		   x
 		 y))
 	     (mapcar
@@ -67,16 +76,19 @@
 	       (mapcar
 		(lambda (x) (list x (count-nestation str x)))
 		(string-find-chars str (string-to-char "\\"))))))))
-      (cl-remove-if-not
-       #'identity
-       (list
-	(car operator-delimiter)
-	(unless (eq (car operator-delimiter) 'not)
-	  (string-trim (substring str 0 (cadr operator-delimiter))))
-	(string-trim (substring str (string-match "\s" str (cadr operator-delimiter)))))))))
+	(cl-remove-if-not
+	 #'identity
+	 (list
+	  (car operator-delimiter)
+	  (unless (eq (car operator-delimiter) 'not)
+	    (string-trim (substring str 0 (cadr operator-delimiter))))
+	  (string-trim
+	   (substring
+	    str
+	    (string-match "\s" str (cadr operator-delimiter))))))))))
 
 (defun string-find-chars (str c)
-  "Get a list with the indices"
+  "Get a list with the indices of where in the string c is present"
   (car
    (cl-reduce
     ;; x = ((1 3 5 6) i)
@@ -90,11 +102,13 @@
 
 (defun is-complete-expression (str)
   "Returns non-nil when str is a complete expression, like 'A' (i.e. when str doesn't contain an operator)"
-  (not (string-match "\\\\" str)))
+  (and
+   (not (listp str)) ;; if str is a list, then of course it's not complete
+   (not (string-match "\\\\" str))))
 
 (defun count-nestation
     (str n)
-  "Counts how many parentheses the nth character is inside"
+  "Counts by how many parentheses the nth character is inside"
   (let
       ((substr (substring str 0 n) ))
     (-
@@ -106,7 +120,10 @@
       substr))))
 
 (defun apply-function (logic left-or-right)
-  "Applies a function"
+  "Applies a function. Logic is a `parse-logic`-like formatted list, with
+   the outermost left or innermost right expression parsed with 
+   parse-expression, depending if left-or-right is 'left or 'right.
+   The target expression must not be a complete expression"
   (let
       ((exp 
 	(cond
@@ -165,15 +182,90 @@
 	  (append (car logic) (cdr exp))
 	  (cdadr logic))))))))
 
+(defun move-expression
+    (exps i left-or-right)
+  "Moves the expression in the index i to either the end or the start"
+  (cond
+   ((eq left-or-right 'left)
+    (append
+     (seq-subseq exps 0 i)
+     (seq-subseq exps (1+ i))
+     (list (nth i exps))))
+   ((eq left-or-right 'right)
+    (cons
+     (nth i exps)
+     (append
+      (seq-subseq exps 0 i)
+      (seq-subseq exps (1+ i)))))))
+
+(defun move-expression-logic (logic i left-or-right)
+  (if (eq left-or-right 'left)
+      (list (move-expression (car logic) i 'left) (cadr logic))
+    (list (car logic) (move-expression (cadr logic) i 'right))))
+
+(defun index-is-functionable (logic i left-or-right)
+    "(I ran out of names). It returns non-nil if the index is 
+at the end of the list of left exps. or at the beginnign of the list of
+right exps."
+    (if (eq left-or-right 'left)
+	(eq i (1- (length (car logic))))
+      (eq i 0)))
+
+(defun step-logic (logic)
+  "Either moves an expression or applies a function and returns the results"
+  (let
+      ((best-exp (get-best-exp logic)))
+    (if (index-is-functionable logic (car best-exp) (cadr best-exp))
+    (apply-function
+     (mapcar
+      (lambda (x)
+	(mapcar #'parse-expression x))
+      logic)
+     (cadr best-exp))
+    (list (move-expression-logic
+       logic
+       (car best-exp)
+       (cadr best-exp)))
+    )))
+
+
+(defun get-best-exp (logic)
+  "Returns a list with the index of the best expression and
+'left or 'right"
+  (let
+      ((left-best
+	(max-cadr
+	 (seq-map-indexed
+	  (lambda (x i) (exp-heuristic x i 'left))
+	  (car logic))))
+       (right-best
+	(max-cadr
+	 (seq-map-indexed
+	  (lambda (x i) (exp-heuristic x i 'right))
+	  (cadr logic)))))
+    (if
+	(> (cadr left-best) (cadr right-best))
+	(list (car left-best) 'left)
+      (list (car right-best) 'right))))
+
+(defun exp-heuristic (exp i left-or-right)
+  "Heurisitc"
+  (list
+   i
+   (cond
+    ((is-complete-expression exp) -10000) ;; if it's a complete exp. it is not the best exp to analyze at all
+    ((eq left-or-right 'left) i) ;; if it's left, higher the index the better it is
+    ((eq left-or-right 'right) (- i))))) ;; if it's on the right, the lower the index the better.
+
+(defun max-cadr (seq)
+  (cl-reduce
+   (lambda (x y)
+     (if
+	 (> (cadr x) (cadr y))
+	 x y))
+   seq))
+
 (message "%S"
-	 (car (apply-function
-	  (mapcar
-	   (lambda (x)
-	     (mapcar
-	      (lambda (x)
-		(parse-expression x))
-	      x))
-	   (parse-logic
-	    "(A \\& C), \\neg (B \\& C) \\vdash "))
-	  'left)))
+	   (mapcar (lambda (x) (step-logic (car x))) (mapcar #'step-logic (step-logic (car (step-logic (car (step-logic (parse-logic
+	    "(A \\& C), \\neg (B \\& C), C, A \\vdash A, B")))))))))
 
